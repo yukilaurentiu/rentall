a0bfb724eb8eee860267800c9f51b52cbd39e9a1
antonio moreno <amorenomtz@gmail.com>
2022-08-29 16:39:59 +0200

    created the product model

---
+++ db/migrate/20220829143905_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :category
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
