d45d7ff26e799ff9608c0d37446adf6224dfdbbb
antonio moreno <amorenomtz@gmail.com>
2022-08-29 17:42:17 +0200

    routing and controller for product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,28 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def show
+    #TODO
+  end
+
+  def new
+    #TODO
+  end
+
+  def create
+    #TODO
+  end
+
+  def edit
+    #TODO
+  end
+
+  def update
+    #TODO
+  end
+
+  def destroy
+    #TODO
+  end
+end
