ebd0041d18084f883f439cadce542e871471cd88
Sonja Peschke <sonjape91@gmail.com>
2022-08-30 14:46:50 +0200

    Added validations for User, Product, Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,20 @@
 class Booking < ApplicationRecord
+  before_create :end_after_start
+
   belongs_to :user
   belongs_to :product
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_price, presence: true, numericality: { only_float: true }
+  validates :user_id, uniqueness: { scope: :product_id }
+
+  private
+
+  # end_date has to be greater than start_date
+  def end_after_start
+    if end_date <= start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  # username is optional
 end
