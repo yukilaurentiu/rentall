66ce56b21172b11877a49ff7707cc064b57890de
antonio moreno <amorenomtz@gmail.com>
2022-08-30 14:21:02 +0200

    navbar and footer partly done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  before_action :fixed_footer, only: [:show, :edit, :mission, :vision]
   def home
+  end
+
+  private
+
+  def fixed_footer
+    @position = "fixed"
   end
 end
