17636ad505cd5c72195af436121952af0d4269ec
Jose Bello <bellojosema11@gmail.com>
2022-08-31 10:13:44 +0100

    Faker validations issue resolved

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
 
   validates :title, presence: true, length:
-    # the user should enter a title that is min. 10 characters and max. 50 characters long
+    #the user should enter a title that is min. 10 characters and max. 50 characters long
     { minimum: 10, maximum: 50, too_long: "%{count} characters is the maximum allowed" }
   validates :description, presence: true, length:
     # the user should enter a description that is min. 10 characters and max. 4000 characters long
-    { minimum: 10, maximum: 4000, too_long: "%{count} characters is the maximum allowed" }
+     {minimum: 10, maximum: 4000, too_long: "%{count} characters is the maximum allowed" }
   validates :category, presence: true
   validates :price, presence: true, numericality: { only_float: true }
 
